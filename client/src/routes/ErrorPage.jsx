4bdc9a42f725f0ebbe0a18fe77601353f8b62f94
Érica Hanemann <ericavhanemann@gmail.com>
2023-10-08 19:14:57 -0300

    feat: error page

--- client/src/routes/ErrorPage.jsx
+++ client/src/routes/ErrorPage.jsx
@@ -1,6 +1,9 @@
 function ErrorPage() {
-    return <div className="mt-20">ErrorPage</div>;
-  }
-  
-  export default ErrorPage;
-  
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-lg">Sorry, something went wrong &#128546;</div>
+    </div>
+  );
+}
+
+export default ErrorPage;
