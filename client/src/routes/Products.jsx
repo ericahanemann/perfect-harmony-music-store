f7f0dcc55f9fd235bb93dd03e43dfdc635f62c40
Érica Hanemann <ericavhanemann@gmail.com>
2023-11-01 13:45:10 -0300

    feat: show products in products page

--- client/src/routes/Products.jsx
+++ client/src/routes/Products.jsx
@@ -13,6 +13,23 @@ function Products() {
     fetchData();
   }, [getAllProducts]);
 
+  const renderedItems = allStockProducts.map((product, index) => {
+    return (
+      <div
+        key={index}
+        className="flex flex-col justify-between items-center w-1/3 p-16 border border-solid"
+      >
+        <div className="flex items-center">
+          <img src={product.colorsAvailable[0].images[0]} alt="product image" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h4>{product.name}</h4>
+          <p>R${product.unitPrice}</p>
+        </div>
+      </div>
+    );
+  });
+
   if (isLoading) {
     return (
       <div className="h-screen w-screen flex justify-center items-center bg-primary text-secondary z-50">
@@ -25,7 +42,15 @@ function Products() {
       </div>
     );
   } else {
-    return <div className="mt-64">Products</div>;
+    return (
+      <div className="mt-64 w-screen mx-auto flex">
+        <div className="w-1/4 flex flex-col justify-start items-center">
+          <div>categories</div>
+          <div>colors</div>
+        </div>
+        <div className="w-3/4 flex flex-wrap mr-24">{renderedItems}</div>
+      </div>
+    );
   }
 }
 
