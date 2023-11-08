96714ca94cb15d6b78d8e6636b8e73da329c38fd
Érica Hanemann <ericavhanemann@gmail.com>
2023-11-08 10:54:17 -0300

    fix: add color propertie

--- server/src/models/products.model.js
+++ server/src/models/products.model.js
@@ -6,6 +6,7 @@ const stockProducts = [
     brand: "fender",
     name: "CLASSIC VIBE '50S STRATOCASTER®",
     description: "very good guitar",
+    color: "#ff0000",
     colorsAvailable: [
       {
         color: "#ff0000",
@@ -32,8 +33,96 @@ const stockProducts = [
     category: "stringed",
     type: "guitar",
     brand: "fender",
+    name: "CLASSIC VIBE '50S STRATOCASTER®",
+    description: "very good guitar",
+    color: "#fff",
+    colorsAvailable: [
+      {
+        color: "#ff0000",
+        images: ["stratocaster-red.png"],
+      },
+      {
+        color: "#fff",
+        images: ["stratocaster-whiteblonde.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["stratocaster-sunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["stratocaster-black.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 3,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
+    name: "CLASSIC VIBE '50S STRATOCASTER®",
+    description: "very good guitar",
+    color: "#3f1e00",
+    colorsAvailable: [
+      {
+        color: "#ff0000",
+        images: ["stratocaster-red.png"],
+      },
+      {
+        color: "#fff",
+        images: ["stratocaster-whiteblonde.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["stratocaster-sunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["stratocaster-black.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 4,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
+    name: "CLASSIC VIBE '50S STRATOCASTER®",
+    description: "very good guitar",
+    color: "#000",
+    colorsAvailable: [
+      {
+        color: "#ff0000",
+        images: ["stratocaster-red.png"],
+      },
+      {
+        color: "#fff",
+        images: ["stratocaster-whiteblonde.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["stratocaster-sunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["stratocaster-black.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 5,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
     name: "PLAYER PLUS METEORA® HH",
     description: "very good guitar",
+    color: "#999",
     colorsAvailable: [
       {
         color: "#999",
@@ -60,12 +149,145 @@ const stockProducts = [
     stockAmount: 12,
   },
   {
-    id: 3,
+    id: 6,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
+    name: "PLAYER PLUS METEORA® HH",
+    description: "very good guitar",
+    color: "#00ff00",
+    colorsAvailable: [
+      {
+        color: "#999",
+        images: ["meteora-silverburst.png"],
+      },
+      {
+        color: "#00ff00",
+        images: ["meteora-cosmicjade.png"],
+      },
+      {
+        color: "#ff0000",
+        images: ["meteora-red.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["meteora-sunburst.png"],
+      },
+      {
+        color: "#0000ff",
+        images: ["meteora-belairblue.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 7,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
+    name: "PLAYER PLUS METEORA® HH",
+    description: "very good guitar",
+    color: "#ff0000",
+    colorsAvailable: [
+      {
+        color: "#999",
+        images: ["meteora-silverburst.png"],
+      },
+      {
+        color: "#00ff00",
+        images: ["meteora-cosmicjade.png"],
+      },
+      {
+        color: "#ff0000",
+        images: ["meteora-red.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["meteora-sunburst.png"],
+      },
+      {
+        color: "#0000ff",
+        images: ["meteora-belairblue.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 8,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
+    name: "PLAYER PLUS METEORA® HH",
+    description: "very good guitar",
+    color: "#3f1e00",
+    colorsAvailable: [
+      {
+        color: "#999",
+        images: ["meteora-silverburst.png"],
+      },
+      {
+        color: "#00ff00",
+        images: ["meteora-cosmicjade.png"],
+      },
+      {
+        color: "#ff0000",
+        images: ["meteora-red.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["meteora-sunburst.png"],
+      },
+      {
+        color: "#0000ff",
+        images: ["meteora-belairblue.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 9,
+    category: "stringed",
+    type: "guitar",
+    brand: "fender",
+    name: "PLAYER PLUS METEORA® HH",
+    description: "very good guitar",
+    color: "#0000ff",
+    colorsAvailable: [
+      {
+        color: "#999",
+        images: ["meteora-silverburst.png"],
+      },
+      {
+        color: "#00ff00",
+        images: ["meteora-cosmicjade.png"],
+      },
+      {
+        color: "#ff0000",
+        images: ["meteora-red.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["meteora-sunburst.png"],
+      },
+      {
+        color: "#0000ff",
+        images: ["meteora-belairblue.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 10,
     category: "stringed",
     type: "guitar",
     brand: "gibson",
     name: "Les Paul Classic",
     description: "very good guitar",
+    color: "#f1d500",
     colorsAvailable: [
       {
         color: "#f1d500",
@@ -88,12 +310,100 @@ const stockProducts = [
     stockAmount: 12,
   },
   {
-    id: 4,
+    id: 11,
+    category: "stringed",
+    type: "guitar",
+    brand: "gibson",
+    name: "Les Paul Classic",
+    description: "very good guitar",
+    color: "#c20000",
+    colorsAvailable: [
+      {
+        color: "#f1d500",
+        images: ["lespaul-honeyburst.png"],
+      },
+      {
+        color: "#c20000",
+        images: ["lespaul-cherry.png"],
+      },
+      {
+        color: "#f1962f",
+        images: ["lespaul-cherrysunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["lespaul-ebony.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 12,
+    category: "stringed",
+    type: "guitar",
+    brand: "gibson",
+    name: "Les Paul Classic",
+    description: "very good guitar",
+    color: "#f1962f",
+    colorsAvailable: [
+      {
+        color: "#f1d500",
+        images: ["lespaul-honeyburst.png"],
+      },
+      {
+        color: "#c20000",
+        images: ["lespaul-cherry.png"],
+      },
+      {
+        color: "#f1962f",
+        images: ["lespaul-cherrysunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["lespaul-ebony.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 13,
+    category: "stringed",
+    type: "guitar",
+    brand: "gibson",
+    name: "Les Paul Classic",
+    description: "very good guitar",
+    color: "#000",
+    colorsAvailable: [
+      {
+        color: "#f1d500",
+        images: ["lespaul-honeyburst.png"],
+      },
+      {
+        color: "#c20000",
+        images: ["lespaul-cherry.png"],
+      },
+      {
+        color: "#f1962f",
+        images: ["lespaul-cherrysunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["lespaul-ebony.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 14,
     category: "stringed",
     type: "guitar",
     brand: "gibson",
     name: "Dave Mustaine Flying V EXP",
     description: "very good guitar",
+    color: "#999",
     colorsAvailable: [
       {
         color: "#999",
@@ -108,12 +418,34 @@ const stockProducts = [
     stockAmount: 12,
   },
   {
-    id: 5,
+    id: 15,
+    category: "stringed",
+    type: "guitar",
+    brand: "gibson",
+    name: "Dave Mustaine Flying V EXP",
+    description: "very good guitar",
+    color: "#f1962f",
+    colorsAvailable: [
+      {
+        color: "#999",
+        images: ["flyingv-silver.png"],
+      },
+      {
+        color: "#f1962f",
+        images: ["flyingv-antiquenatural.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 16,
     category: "percussion",
     type: "drums",
     brand: "gibson",
     name: "drums 1",
     description: "very good drums",
+    color: "#ff0000",
     colorsAvailable: [
       {
         color: "#ff0000",
@@ -136,12 +468,71 @@ const stockProducts = [
     stockAmount: 12,
   },
   {
-    id: 6,
+    id: 17,
     category: "percussion",
     type: "drums",
     brand: "gibson",
-    name: "drums 2",
+    name: "drums 1",
     description: "very good drums",
+    color: "#fff",
+    colorsAvailable: [
+      {
+        color: "#ff0000",
+        images: ["stratocaster-red.png"],
+      },
+      {
+        color: "#fff",
+        images: ["stratocaster-whiteblonde.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["stratocaster-sunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["stratocaster-black.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 18,
+    category: "percussion",
+    type: "drums",
+    brand: "gibson",
+    name: "drums 1",
+    description: "very good drums",
+    color: "#3f1e00",
+    colorsAvailable: [
+      {
+        color: "#ff0000",
+        images: ["stratocaster-red.png"],
+      },
+      {
+        color: "#fff",
+        images: ["stratocaster-whiteblonde.png"],
+      },
+      {
+        color: "#3f1e00",
+        images: ["stratocaster-sunburst.png"],
+      },
+      {
+        color: "#000",
+        images: ["stratocaster-black.png"],
+      },
+    ],
+    unitPrice: 1500,
+    stockAmount: 12,
+  },
+  {
+    id: 19,
+    category: "percussion",
+    type: "drums",
+    brand: "gibson",
+    name: "drums 1",
+    description: "very good drums",
+    color: "#000",
     colorsAvailable: [
       {
         color: "#ff0000",
